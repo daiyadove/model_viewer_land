1be9f4f633b191886b4fe4b8293d53dcb5e2bd84
daiya <phaosdove@gmail.com>
2020-05-24 21:55:40 +0900

    大文字ではない

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import ModelList from '../components/ModelList'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import Head from 'next/head';
 
 const Index = () => (
