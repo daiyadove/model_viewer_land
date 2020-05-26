77899a16064334bda78ba2f0e50fb0e4e771bd34
daiya <phaosdove@gmail.com>
2020-05-26 23:41:37 +0900

    data.jsonから読み込む

--- components/ModelList.jsx
+++ components/ModelList.jsx
@@ -1,18 +1,17 @@
 import ModelItem from '../components/ModelItem'
+import data from '../data/data.json'
 
-const ModelList = () => (
-  <div>
-    <ModelItem
-      src='https://firebasestorage.googleapis.com/v0/b/anyr-land.appspot.com/o/eva%2Fout.glb?alt=media&token=1eec0063-b796-48b5-9fcd-a228bd1d26ba'
-      iosSrc='https://firebasestorage.googleapis.com/v0/b/anyr-land.appspot.com/o/eva%2Fusdz%2FEva01.usdz?alt=media&token=e8900293-8bb2-4865-969e-5397b26a4fb3'
-      alt='Eva 01'
-    />
-    <ModelItem
-      src='https://firebasestorage.googleapis.com/v0/b/anyr-land.appspot.com/o/miltank_the_thicc_pokemon%2Fmiltank_the_thicc_pokemon.glb?alt=media&token=8d0ea8d5-830b-4e25-9387-a6a8e4cdba02'
-      iosSrc='https://firebasestorage.googleapis.com/v0/b/anyr-land.appspot.com/o/miltank_the_thicc_pokemon%2FMiltank_the_thicc_Pokemon.usdz?alt=media&token=e53acf01-bf85-41a1-bfa8-3160c7b274c0'
-      alt='miltank_the_thicc_pokemon'
-    />
-  </div>
-)
-
+const ModelList = () => {
+  return (
+    <ul>
+      {data.models.map((d, idx) => {
+        return <ModelItem key={idx}
+          iosSrc={d.iosSrc}
+          src={d.src}
+          alt={d.alt}
+        />
+      })}
+    </ul>
+  )
+}
 export default ModelList
