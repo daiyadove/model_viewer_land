1396217e2143acf0b133214228e0fe730fad5b72
daiya <phaosdove@gmail.com>
2020-06-02 22:10:37 +0900

    一旦完成

---
+++ pages/model/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <ModelItem
+    :src="model.src"
+    :iosSrc="model.iosSrc"
+    :title="model.alt"
+    :ccBy="model.ccBy"
+  />
+</template>
+
+<script>
+import '@google/model-viewer/dist/model-viewer'
+import { defineComponent } from '@vue/composition-api'
+import ModelItem from '@/components/ModelItem'
+import data from '@/data/data.json'
+export default defineComponent({
+  components: {
+    ModelItem
+  },
+  setup (_, context) {
+    const id = context.root.$route.params.id
+    const models = data.models
+    const model = models.find(model => Number(model.id) === Number(id))
+    return {
+      model
+    }
+  }
+})
+</script>
+
+<style>
+</style>
